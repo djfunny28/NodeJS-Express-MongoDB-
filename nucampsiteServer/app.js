c6bf851de84ebb8205f5778cc68d62e634c9898c
djfunny28 <shekonio@gmail.com>
2021-07-16 17:08:10 -0700

    Favorite Campsite Rest Api

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -13,6 +13,7 @@ const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
@@ -58,6 +59,7 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/nucampsite',
     'facebook': {
-        clientId: 'YOUR FACEBOOK APP ID HERE',
-        clientSecret: 'YOUR FACEBOOK APP SECRET HERE'
+        clientId: '1921494164675713',
+        clientSecret: '8cbe196504450e5501dd3c8effd9da0a'
     }
 }
